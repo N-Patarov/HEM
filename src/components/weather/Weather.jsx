13c56d02bcf06b4583e25e0fd3e565114731ab72
niki <nikolayp1707@gmail.com>
2021-11-26 21:50:19 +0200

    add more stuff to  weather

--- src/components/weather/Weather.jsx
+++ src/components/weather/Weather.jsx
@@ -3,9 +3,9 @@ import styles from "./Weather.module.scss";
 export default function Weather({degrees,type=""}){
     return(
         <div className={styles['weather-wrapper']}>
-            <div className={styles['image']}>image</div>
-            <div className={styles['weather']}>weather</div>
-            <div className={styles['deg']}>degrees</div>
+            <div className={styles['image']} >image</div>
+            <Typography className={styles['weather']}>weather</Typography>
+            <Typography variant={<h1 />} className={styles['deg']}>degrees</Typography>
         </div>
     );
 }
