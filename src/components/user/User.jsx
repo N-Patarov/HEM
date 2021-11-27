4fdb615c0928036f0abc744f5b711aa4f7872eee
Volen Nakov <volen.nakov@gmail.com>
2021-11-27 21:20:11 +0200

    fix: User component

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -4,8 +4,8 @@ import classNames from "classnames";
 
 export default function User({ avatar, name, size }) {
     return (
-        <div>
-            <Container className={classNames(styles['user-container'])}>
+        <div className={classNames(styles['user-container'])}>
+            <Container >
                 <Grid container alignItems="center" justifyContent="center" spacing={1}>
                     <Grid item><Avatar src={avatar} sx={{ width: size, height: size }} alt="Avatar picture" /></Grid>
                     <Grid item ><Typography variant="h5">{name}</Typography></Grid>
