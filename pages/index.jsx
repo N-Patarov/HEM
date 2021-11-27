02cf7ea797f398675b46ab27b4db8c281ea6573f
Yancho Kostov <yankostov1@gmail.com>
2021-11-27 14:26:39 +0200

    feat: add LoginPage

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,7 @@
 import Example from "../src/components/example/Example";
 import Login from "../src/components/login/Login";
+import LoginPage from "./login/index";
+
 export default function Index() {
-  return <Login />;
+  return <LoginPage />;
 }
