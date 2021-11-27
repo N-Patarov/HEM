0c42333395fd3d2302cf19566d324f4f241fa847
Volen Nakov <volen.nakov@gmail.com>
2021-11-27 17:25:12 +0200

    fix: User component

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -1,12 +1,13 @@
 import styles from "./User.module.scss"
 import { Container, Grid, Avatar, Typography } from "@mui/material"
+import classNames from "classnames";
 
-export default function User({ avatar, username, size }) {
+export default function User({ avatar, name, size }) {
     return (
-        <Container className={styles['user-container']}>
+        <Container className={classNames(styles['user-container'])}>
             <Grid container alignItems="center" justifyContent="center" spacing={1}>
                 <Grid item><Avatar src={avatar} sx={{ width: size, height: size }} alt="Avatar picture" /></Grid>
-                <Grid item ><Typography variant="h5">{username}</Typography></Grid>
+                <Grid item ><Typography variant="h5">{name}</Typography></Grid>
             </Grid>
         </Container>
     )
