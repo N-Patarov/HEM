5d954bb2f43ef3a594aa33b435fc940e80424d72
Yancho Kostov <yankostov1@gmail.com>
2021-11-30 17:35:27 +0200

    feat: create navigation .jsx and .scss

---
+++ src/components/navigation/Navigation.jsx
@@ -0,0 +1,35 @@
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import ListItem from "@mui/material/ListItem";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Badge from "@mui/material/Badge";
+import ListItemText from "@mui/material/ListItemText";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import BedOutlinedIcon from "@mui/icons-material/BedOutlined";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import User from "../user/User";
+
+import * as React from 'react';
+import Toolbar from "@mui/material/Toolbar";
+import Divider from '@mui/material/Divider';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
+import Container from "@mui/material/Container";
+import styles from "./Navigation.module.scss";
+import classNames from "classnames";
+
+
+
+export default function Navigation() {
+
+
+  return (
+    <div className={classNames(styles.wrapper)}>
+      
+    </div>
+  );
+}
