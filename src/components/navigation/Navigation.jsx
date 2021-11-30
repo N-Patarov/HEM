a674f3319d4d2309eaf5dbe440448e655190894b
Yancho Kostov <yankostov1@gmail.com>
2021-11-30 17:41:56 +0200

    feat: add Toolbar

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -54,6 +54,47 @@ export default function Navigation() {
         anchor="left"
       >
 
+
+        <Toolbar >
+
+          <User avatar="images/avatar.png" name="John Doe" />
+
+          <IconButton
+            aria-label="more"
+            id="long-button"
+            aria-controls="long-menu"
+            aria-expanded={open ? 'true' : undefined}
+            aria-haspopup="true"
+            onClick={handleClick}
+          >
+            <MoreVertIcon />
+          </IconButton>
+          <Menu
+            id="long-menu"
+            MenuListProps={{
+              'aria-labelledby': 'long-button',
+            }}
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            PaperProps={{
+              style: {
+                maxHeight: ITEM_HEIGHT * 4.5,
+                width: '20ch',
+              },
+            }}
+          >
+            {options.map((option) => (
+              <MenuItem key={option} selected={option === 'Pyxis'}
+                onClick={handleClose}>
+                {option}
+              </MenuItem>
+            ))}
+          </Menu>
+
+        </Toolbar>
+
+
         <Divider />
         <List>
           {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
