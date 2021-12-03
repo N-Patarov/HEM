01cc4b49f1933b047a663f8cdae2c0eb4bc0dff0
Yancho Kostov <yankostov1@gmail.com>
2021-12-03 12:39:49 +0200

    feet: add scenes

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,8 +6,36 @@ import Weather from "../src/components/weather/Weather";
 import Time from "../src/components/time/Time";
 import LoginPage from "./login/index";
 import Navigation from "../src/components/navigation/Navigation";
+import Card from "../src/components/card/Card";
+import Scenes from "../src/components/scenes/Scenes";
+
+const cardsData=[
+  {
+    iconUrl:'./images/morning.svg',
+    outlined: false
+  },
+  {
+    iconUrl:'./images/alarm-clock.svg',
+    outlined: false
+  },
+  {
+    iconUrl:'./images/shower.svg',
+    outlined: false
+  },
+  {
+    iconUrl:'./images/tea-cup.svg',
+    outlined: false
+  },
+  {
+    iconUrl:'./images/rock.svg',
+    outlined: false
+  },
+  {
+    iconUrl:'./images/plus.svg',
+    outlined: true
+  },
+];
 
 export default function Index() {
-  return <Navigation/>;
-
+  return <Scenes cards={cardsData}/>;
 }
