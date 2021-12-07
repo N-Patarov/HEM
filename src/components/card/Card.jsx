a0ddaaac7b010c86d98625eaa6e38b289b0591f3
Volen Nakov <volen.nakov@gmail.com>
2021-12-07 11:56:10 +0200

    fix: Changing the Card css

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,17 +1,17 @@
 import { Card as MuiCard, CardContent, ButtonBase } from "@mui/material"
-import style from "./Card.module.scss"
+import styles from "./Card.module.scss"
 import classNames from "classnames"
 export default function Card({ iconUrl = false, outlined = false, onClick }) {
     return (
-        <div className={classNames(style.card)}>
+        <div className={classNames(styles.card)}>
             <MuiCard sx={(outlined) ? {
                 border: "3px solid #7441F3",
                 boxSizing: "border-box",
                 borderRadius: "20px"
-            } : ""}>
+            } : ""} className={classNames(styles.muiCard)}>
                 <ButtonBase onClick={onClick}>
-                    <CardContent className={classNames(style.content)}>
-                        {(iconUrl) ? <img src={iconUrl}></img> : ""}
+                    <CardContent className={classNames(styles.content)}>
+                        {(iconUrl) ? <img src={iconUrl} className={classNames(styles.image)}></img> : ""}
                     </CardContent>
                 </ButtonBase>
             </MuiCard>
