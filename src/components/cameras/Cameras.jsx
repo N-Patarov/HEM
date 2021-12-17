159ad742c4b4f123f6b26b4b4d9c056c11359493
Volen Nakov <volen.nakov@gmail.com>
2021-12-17 02:12:57 +0200

    fix: Fixing cameras componentn

--- src/components/cameras/Cameras.jsx
+++ src/components/cameras/Cameras.jsx
@@ -1,40 +1,42 @@
 import classNames from "classnames"
 import styles from "./Cameras.module.scss"
 import Card from "../card/Card"
-import { useState } from 'react';
-import { Grid, CardMedia, Card as MuiCard, ButtonBase, Box } from "@mui/material"
+import { useState, useEffect } from 'react';
+import { Grid, Box } from "@mui/material"
 
-export default function Cameras({ cameras, hasButton }) {
+export default function Cameras({ cameras, hasButton = false }) {
     const [selected, setSelected] = useState(cameras[0].videoUrl)
+    useEffect(() => {
+        let player = document.querySelector('#player')
+        player.load()
+        return () => {
+            player.pause()
+        }
+    }, [selected])
     const parsedCameras = cameras.map((camera) =>
         <Grid item xs={6}>
-            <MuiCard>
-                <ButtonBase onClick={() => { setSelected(camera.videoUrl) }}>
-                    <CardMedia component="video" src={camera.videoUrl} sx={(selected == camera.videoUrl) ? {
-                        borderRadius: '15px', border: '5px solid #7441F3',
-                    } : { borderRadius: '15px', }} />
-                </ButtonBase>
-            </MuiCard>
+            <video onClick={() => { setSelected(camera.videoUrl) }} className={classNames(styles.videoButton)}>
+                <source src={camera.videoUrl} type="video/mp4" />
+            </video>
         </Grid >
     )
-
     return (
         <div className={classNames(styles.container)}>
             <Grid container spacing={2} >
                 <Grid item xs={4} >
-                    <CardMedia component="video" src={selected} style={{
-                        borderRadius: '20px',
-                    }} autoPlay muted />
+                    <video id='player' className={classNames(styles.videoButton)} autoPlay muted loop>
+                        <source src={selected} type="video/mp4" />
+                    </video>
                 </Grid>
-                <Grid item container={(hasButton) ? true : false} xs={2} spacing={1} direction="column" >
-                    <Grid item>
-                        <Box sx={{ maxHeight: '300px', overflow: "auto", }}>
+                <Grid item xs={2} spacing={1} direction="column" >
+                    <Grid container>
+                        <Box sx={{ maxHeight: '500px', overflow: "auto", }}>
                             <Grid container spacing={1}>
                                 {parsedCameras}
                             </Grid>
                         </Box>
+                        {(hasButton) ? <Grid item sx={{width:'100%'}}><Card iconUrl={'images/plus.svg'}/></Grid> : null}
                     </Grid>
-                    {(hasButton) ? <Grid item xs={1}><Card outlined={true} iconUrl="images/plus.svg" /></Grid> : null}
                 </Grid>
             </Grid>
         </div >
